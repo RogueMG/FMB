77841e83b3b36833c151c97a656a044f1a195719
Murtaza Moiz <98089493+RogueMG@users.noreply.github.com>
2025-04-02 17:56:38 +0530

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -98,11 +98,11 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("DB_NAME"),
-        "USER": os.getenv("DB_USER"),
-        "PASSWORD": os.getenv("DB_PWD"),
-        "HOST": os.getenv("DB_HOST"),
-        "PORT": os.getenv("DP_PORT"),
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "root",
+        "HOST": "",
+        "PORT": "5432",
     }
 }
 
